77c7986c6908e8586ae70389b34585991444bbdf
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2019-12-29 22:28:18 +0530

    Add the fizzbuzz problem

---
+++ fizzBuzz/index.js
@@ -0,0 +1,15 @@
+function fizzBuzz(n) {
+  for (i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+module.exports = fizzBuzz;
