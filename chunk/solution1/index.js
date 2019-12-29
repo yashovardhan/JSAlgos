290bf4523440d910b6ccb67d9366e5a44b31bfaa
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2019-12-29 23:47:28 +0530

    Add array chunk problem

---
+++ chunk/solution1/index.js
@@ -0,0 +1,16 @@
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
+
+module.exports = chunk;
---
+++ chunk/solution2/index.js
@@ -0,0 +1,9 @@
+function chunk(array, size) {
+  const chunked = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunked.push(array.slice(i, i + size))
+  }
+  return chunked;
+}
+
+module.exports = chunk;
