549892769144de38d641083d993d8925eca595a9
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2020-01-28 14:06:11 +0530

    sorting: fix typo

--- sorting/index.js
+++ sorting/index.js
@@ -32,7 +32,7 @@ function mergeSort(arr) {
   
   const midpoint = Math.floor(arr.length / 2);
   const left = arr.slice(0, midpoint);
-  const right = arr.splice(midpoint);
+  const right = arr.slice(midpoint);
 
   return merge(mergeSort(left), mergeSort(right));
 }
